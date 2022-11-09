0c2a22158e8cae68c182a3072b19a2f41235cd95
Saifuddinmonna <Saifuddinmonna@gmail.com>
2022-11-09 23:59:15 +0600

    service name added finaly and successfully

--- index.js
+++ index.js
@@ -37,87 +37,101 @@ const client = new MongoClient(uri, {
 // 	});
 // }
 
-async function run() {
+const serviceName = client.db("repairService").collection("servicesName");
+async function dbconnect() {
 	try {
 		await client.connect();
 
-		const serviceName = client
-			.db("repairService")
-			.collection("servicesName");
+		
 		console.log("db connected sucessfully".green);
-		const order = {
-			name: "Anuar",
-			job:" bank"
-		}
-		const result = await serviceName.insertOne(order);
-		// 			res.send(result);
-	} finally { }
+	}
+	catch (error) {
+		console.log(error.name.bgRed,error.message.bold)
+	}
+}
+ 
+dbconnect();
+
 	
 		
-}
-	run().catch(console.dir);
+		
 
-		// 		app.post("/jwt", (req, res) => {
-		// 			const user = req.body;
-		// 			const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-		// 				expiresIn: "1d",
-		// 			});
-		// 			res.send({ token });
-		// 		});
-		// 		app.get("/services", async (req, res) => {
-		// 			const query = {};
-		// 			const cursor = serviceCollection.find(query);
-		// 			const services = await cursor.toArray();
-		// 			res.send(services);
-		// 		});
-		// 		app.get("/services/:id", async (req, res) => {
-		// 			const id = req.params.id;
-		// 			const query = { _id: ObjectId(id) };
-		// 			const service = await serviceCollection.findOne(query);
-		// 			res.send(service);
-		// 		});
-		// 		// orders api
-		// 		app.get("/orders", verifyJWT, async (req, res) => {
-		// 			const decoded = req.decoded;
-		// 			if (decoded.email !== req.query.email) {
-		// 				res.status(403).send({ message: "unauthorized access" });
-		// 			}
-		// 			let query = {};
-		// 			if (req.query.email) {
-		// 				query = {
-		// 					email: req.query.email,
-		// 				};
-		// 			}
-		// 			const cursor = orderCollection.find(query);
-		// 			const orders = await cursor.toArray();
-		// 			res.send(orders);
-		// 		});
-		// 		app.post("/orders", verifyJWT, async (req, res) => {
-		// 			const order = req.body;
-		// 			const result = await orderCollection.insertOne(order);
-		// 			res.send(result);
-		// 		});
-		// 		app.patch("/orders/:id", verifyJWT, async (req, res) => {
-		// 			const id = req.params.id;
-		// 			const status = req.body.status;
-		// 			const query = { _id: ObjectId(id) };
-		// 			const updatedDoc = {
-		// 				$set: {
-		// 					status: status,
-		// 				},
-		// 			};
-		// 			const result = await orderCollection.updateOne(query, updatedDoc);
-		// 			res.send(result);
-		// 		});
-		// 		app.delete("/orders/:id", verifyJWT, async (req, res) => {
-		// 			const id = req.params.id;
-		// 			const query = { _id: ObjectId(id) };
-		// 			const result = await orderCollection.deleteOne(query);
-		// 			res.send(result);
-		// 		});
-		// }
-	
+		app.get("/servicename", async (req, res) => {
+			try {
+				const query = { runtime: { $lt: 15 } };
+				const cursor = serviceName.find();
+				const services = await cursor.toArray();
+				res.send(services);
 
+				
+			}
+		 catch (error) {
+		console.log( "i got a errrr".bgRed);
+	        }
+		});
+		
+
+
+
+// 		app.post("/jwt", (req, res) => {
+// 			const user = req.body;
+// 			const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+// 				expiresIn: "1d",
+// 			});
+// 			res.send({ token });
+// 		});
+// 		app.get("/services", async (req, res) => {
+// 			const query = {};
+// 			const cursor = serviceCollection.find(query);
+// 			const services = await cursor.toArray();
+// 			res.send(services);
+// 		});
+// 		app.get("/services/:id", async (req, res) => {
+// 			const id = req.params.id;
+// 			const query = { _id: ObjectId(id) };
+// 			const service = await serviceCollection.findOne(query);
+// 			res.send(service);
+// 		});
+// 		// orders api
+// 		app.get("/orders", verifyJWT, async (req, res) => {
+// 			const decoded = req.decoded;
+// 			if (decoded.email !== req.query.email) {
+// 				res.status(403).send({ message: "unauthorized access" });
+// 			}
+// 			let query = {};
+// 			if (req.query.email) {
+// 				query = {
+// 					email: req.query.email,
+// 				};
+// 			}
+// 			const cursor = orderCollection.find(query);
+// 			const orders = await cursor.toArray();
+// 			res.send(orders);
+// 		});
+// 		app.post("/orders", verifyJWT, async (req, res) => {
+// 			const order = req.body;
+// 			const result = await orderCollection.insertOne(order);
+// 			res.send(result);
+// 		});
+// 		app.patch("/orders/:id", verifyJWT, async (req, res) => {
+// 			const id = req.params.id;
+// 			const status = req.body.status;
+// 			const query = { _id: ObjectId(id) };
+// 			const updatedDoc = {
+// 				$set: {
+// 					status: status,
+// 				},
+// 			};
+// 			const result = await orderCollection.updateOne(query, updatedDoc);
+// 			res.send(result);
+// 		});
+// 		app.delete("/orders/:id", verifyJWT, async (req, res) => {
+// 			const id = req.params.id;
+// 			const query = { _id: ObjectId(id) };
+// 			const result = await orderCollection.deleteOne(query);
+// 			res.send(result);
+// 		});
+// }
 
 // run().catch((err) => console.error(err));
 
