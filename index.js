b9afa82b8e32340397f91fb508f16be787a5d72a
Saifuddinmonna <Saifuddinmonna@gmail.com>
2022-11-09 18:37:22 +0600

    user and password added

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.twtll.mongodb.net/?retryWrites=true&w=majority`;
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wuiqqjn.mongodb.net/?retryWrites=true&w=majority`;
+// const uri =
+// 	"mongodb+srv://<username>:<password>@cluster0.wuiqqjn.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
