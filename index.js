edac58d351962aad3ccbebdca70015968d2d4947
Saifuddinmonna <Saifuddinmonna@gmail.com>
2022-11-09 21:16:23 +0600

    connested sucseesfully and insert documnt

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
-
+var colors = require("colors");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -20,104 +20,111 @@ const client = new MongoClient(uri, {
 	serverApi: ServerApiVersion.v1,
 });
 
-function verifyJWT(req, res, next) {
-	const authHeader = req.headers.authorization;
+// function verifyJWT(req, res, next) {
+// 	const authHeader = req.headers.authorization;
 
-	if (!authHeader) {
-		return res.status(401).send({ message: "unauthorized access" });
-	}
-	const token = authHeader.split(" ")[1];
+// 	if (!authHeader) {
+// 		return res.status(401).send({ message: "unauthorized access" });
+// 	}
+// 	const token = authHeader.split(" ")[1];
 
-	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-		if (err) {
-			return res.status(403).send({ message: "Forbidden access" });
-		}
-		req.decoded = decoded;
-		next();
-	});
-}
+// 	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+// 		if (err) {
+// 			return res.status(403).send({ message: "Forbidden access" });
+// 		}
+// 		req.decoded = decoded;
+// 		next();
+// 	});
+// }
 
 async function run() {
 	try {
-		const serviceCollection = client.db("geniusCar").collection("services");
-		const orderCollection = client.db("geniusCar").collection("orders");
+		await client.connect();
 
-		app.post("/jwt", (req, res) => {
-			const user = req.body;
-			const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-				expiresIn: "1d",
-			});
-			res.send({ token });
-		});
-
-		app.get("/services", async (req, res) => {
-			const query = {};
-			const cursor = serviceCollection.find(query);
-			const services = await cursor.toArray();
-			res.send(services);
-		});
-
-		app.get("/services/:id", async (req, res) => {
-			const id = req.params.id;
-			const query = { _id: ObjectId(id) };
-			const service = await serviceCollection.findOne(query);
-			res.send(service);
-		});
-
-		// orders api
-		app.get("/orders", verifyJWT, async (req, res) => {
-			const decoded = req.decoded;
-
-			if (decoded.email !== req.query.email) {
-				res.status(403).send({ message: "unauthorized access" });
-			}
-
-			let query = {};
-			if (req.query.email) {
-				query = {
-					email: req.query.email,
-				};
-			}
-			const cursor = orderCollection.find(query);
-			const orders = await cursor.toArray();
-			res.send(orders);
-		});
-
-		app.post("/orders", verifyJWT, async (req, res) => {
-			const order = req.body;
-			const result = await orderCollection.insertOne(order);
-			res.send(result);
-		});
-
-		app.patch("/orders/:id", verifyJWT, async (req, res) => {
-			const id = req.params.id;
-			const status = req.body.status;
-			const query = { _id: ObjectId(id) };
-			const updatedDoc = {
-				$set: {
-					status: status,
-				},
-			};
-			const result = await orderCollection.updateOne(query, updatedDoc);
-			res.send(result);
-		});
-
-		app.delete("/orders/:id", verifyJWT, async (req, res) => {
-			const id = req.params.id;
-			const query = { _id: ObjectId(id) };
-			const result = await orderCollection.deleteOne(query);
-			res.send(result);
-		});
-	} finally {
-	}
+		const serviceName = client
+			.db("repairService")
+			.collection("servicesName");
+		console.log("db connected sucessfully".green);
+		const order = {
+			name: "Anuar",
+			job:" bank"
+		}
+		const result = await serviceName.insertOne(order);
+		// 			res.send(result);
+	} finally { }
+	
+		
 }
+	run().catch(console.dir);
 
-run().catch((err) => console.error(err));
+		// 		app.post("/jwt", (req, res) => {
+		// 			const user = req.body;
+		// 			const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+		// 				expiresIn: "1d",
+		// 			});
+		// 			res.send({ token });
+		// 		});
+		// 		app.get("/services", async (req, res) => {
+		// 			const query = {};
+		// 			const cursor = serviceCollection.find(query);
+		// 			const services = await cursor.toArray();
+		// 			res.send(services);
+		// 		});
+		// 		app.get("/services/:id", async (req, res) => {
+		// 			const id = req.params.id;
+		// 			const query = { _id: ObjectId(id) };
+		// 			const service = await serviceCollection.findOne(query);
+		// 			res.send(service);
+		// 		});
+		// 		// orders api
+		// 		app.get("/orders", verifyJWT, async (req, res) => {
+		// 			const decoded = req.decoded;
+		// 			if (decoded.email !== req.query.email) {
+		// 				res.status(403).send({ message: "unauthorized access" });
+		// 			}
+		// 			let query = {};
+		// 			if (req.query.email) {
+		// 				query = {
+		// 					email: req.query.email,
+		// 				};
+		// 			}
+		// 			const cursor = orderCollection.find(query);
+		// 			const orders = await cursor.toArray();
+		// 			res.send(orders);
+		// 		});
+		// 		app.post("/orders", verifyJWT, async (req, res) => {
+		// 			const order = req.body;
+		// 			const result = await orderCollection.insertOne(order);
+		// 			res.send(result);
+		// 		});
+		// 		app.patch("/orders/:id", verifyJWT, async (req, res) => {
+		// 			const id = req.params.id;
+		// 			const status = req.body.status;
+		// 			const query = { _id: ObjectId(id) };
+		// 			const updatedDoc = {
+		// 				$set: {
+		// 					status: status,
+		// 				},
+		// 			};
+		// 			const result = await orderCollection.updateOne(query, updatedDoc);
+		// 			res.send(result);
+		// 		});
+		// 		app.delete("/orders/:id", verifyJWT, async (req, res) => {
+		// 			const id = req.params.id;
+		// 			const query = { _id: ObjectId(id) };
+		// 			const result = await orderCollection.deleteOne(query);
+		// 			res.send(result);
+		// 		});
+		// }
+	
+
+
+// run().catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
-	res.send("genius car server is running");
+	res.send("repair service is running");
 });
 
 app.listen(port, () => {
-	console.log(`Genius Car server running on ${port}`);
+	console.log(`repair service running on ${port}`);
 });
